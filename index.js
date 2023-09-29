a95b7d76358603e38de65c1a5c38985ff5678643
Ali Faizan <alifaizan1983@gmail.com>
2023-09-29 12:20:54 +0530

    added -m method

--- index.js
+++ index.js
@@ -24,7 +24,7 @@ function countLines(filePath) {
   const rl = readline.createInterface({
     input: fs.createReadStream(filePath),
     output: process.stdout,
-    terminal: false
+    terminal: false,
   });
 
   let lineCount = 0;
@@ -36,6 +36,38 @@ function countLines(filePath) {
   rl.on("close", () => {
     console.log(`${lineCount} ${filePath}`);
   });
+
+  rl.on("error", (error) => {
+    console.error(`No such file or directory`);
+    process.exit(1);
+  });
+}
+
+//Function to count the words in a file
+function countWords(filePath) {
+  try {
+    const data = fs.readFileSync(filePath, "utf-8");
+    const words = data.split(/\s+/).filter((word) => word.trim() !== "");
+    words.forEach((word, index) => {
+      // console.log(`${index + 1} ${word}`);
+    });
+    console.log(`${words.length} ${filePath}`);
+  } catch (error) {
+    console.error(`Error reading ${filePath}: ${error}`);
+    process.exit(1);
+  }
+}
+
+// Function to count ch in a file
+function countCharacters(filePath) {
+  try {
+    const content = fs.readFileSync(filePath, "utf-8");
+    const characterCount = content.length;
+    console.log(`${characterCount} ${filePath}`);
+  } catch (error) {
+    console.error(`No such file or directory`);
+  }
+  process.exit(1);
 }
 
 // Extracting command-line arguments, excluding the first two elements (node and script name)
@@ -57,16 +89,30 @@ switch (args[0]) {
     }
     countLines(args[1]);
     break;
+  case "-w":
+    if (args.length != 2 || args[0] !== "-w") {
+      console.error("Usage: ccwc -w <file>");
+      process.exit(1);
+    }
+    countWords(args[1]);
+    break;
+  case "-m":
+    if (args.length != 2 || args[0] !== "-m") {
+      console.error("Usage: ccwc -m <file>");
+      process.exit(1);
+    }
+    countCharacters(args[1]);
+    break;
   case "-help":
     console.log(`Usage: ccwc [options] <file>
 
       Options:
         -c, --bytes    Print the byte counts
-        -m, --chars    Print the character counts
         -l, --lines    Print the newline counts
+        -w  --words    Print the word counts
+        -m, --chars    Print the character counts
       `);
     break;
   default:
     console.error("check command by using -help");
-    process.exit(1);
 }
